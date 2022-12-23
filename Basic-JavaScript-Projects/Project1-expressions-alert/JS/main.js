6fc2b17ed8170a82bc4f398ee4ab0bc6f323bd3b
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-23 19:58:39 -0400

    Basic JavaScript Projects Submission Assignment

--- Basic-JavaScript-Projects/Project1-expressions-alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1-expressions-alert/JS/main.js
@@ -1,1 +1,48 @@
+//Basic JavaScript Projects Submission Assignment
+
+//Alert window with a message
 window.alert ("Hello, World! This is an alert message made with JavaScript for HTML");
+
+//Writing a line of text with document method
+document.write ("This is a document write method test text")
+
+//Creating Variables
+var Sent1="This is the begining of the string" //Defining a variable and assigning it a string value
+var Sent2=" and this is the end of the string" //Defining a variable and assigning it a string value
+ document.write(Sent1 + Sent2) //Concanating the two variables
+
+//Expression example
+10 + 24
+
+
+
+
+
+
+//Assigning a string varable and printing it out
+var A="This is a string";
+document.write (A);
+
+//Escape Character Assignment
+document.write ("The doctor said, \"Stop being an ass Sally and get to work!\"")
+
+//concenating (connecting two) strings
+document.write ("\"Be who you are and say what you feel," + " because those who mind don't matter" + " and those who matter don't mind.\"-Dr. Seuss")
+
+//Multiple vairable
+var Family = "The Ghaisens", Dad="Paul", Mom="Agata", Daughter="Christine", Son="William";
+document.write(Daughter)
+
+//expression
+3+3
+
+//Statement
+document.write(3+3);
+
+function My_First_function() { //Defining a function and naming it
+    var str="This is the button text!" //Defining a variable and assigning it a string value
+
+document.getElementById("Button_text").innerHTML=str; //Putting the value of the 
+                                                    //variable into the HTML ElementFromPoint
+                                                    //with the button
+}
