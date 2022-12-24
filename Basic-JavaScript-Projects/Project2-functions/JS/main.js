e33ba202edfd426a7563c51585899af8bcb2d4c9
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-23 20:25:33 -0400

    Function Assignment

---
+++ Basic-JavaScript-Projects/Project2-functions/JS/main.js
@@ -0,0 +1,7 @@
+function My_Function() { //Defining a function and naming it
+    var Text1="This is the first variable"; //Defining a variable and assigning it a string value
+    var Text2="This is the seccond variable"; //Defining a seccond variable and assigning it a string value
+document.getElementById("button").innerHTML=Text1 //Putting the value of the 
+                                                    //variable into the HTML ElementFromPoint
+                                                    //with the button
+}
