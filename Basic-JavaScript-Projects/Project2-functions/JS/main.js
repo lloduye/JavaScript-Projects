56fb9aec60aa916dccb5488bc4fee640ee164b1b
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-24 03:15:48 -0400

    Project 2 assignment Complete and code working

--- Basic-JavaScript-Projects/Project2-functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2-functions/JS/main.js
@@ -1,7 +1,6 @@
-function My_Function() { //Defining a function and naming it
-    var Text1="This is the first variable"; //Defining a variable and assigning it a string value
-    var Text2="This is the seccond variable"; //Defining a seccond variable and assigning it a string value
-document.getElementById("button").innerHTML=Text1 //Putting the value of the 
-                                                    //variable into the HTML ElementFromPoint
-                                                    //with the button
+function myCode() { // Defining a function and assigning it a name
+    var reason="Coding is something that makes a difference."; //Defining a variable and assigning it a value
+    reason +=" When I code, I get this feeling that you I'm solving a problem"; //using the +=operator to concatate the two strings of sentences
+    document.getElementById("joinsent").innerHTML=reason; //putting the value of the variable
+                                                        //into the HTML with a paragraph element
 }
