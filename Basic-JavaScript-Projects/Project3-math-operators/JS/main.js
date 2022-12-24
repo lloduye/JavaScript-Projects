5a8e3f613ec224eaeada0d6c7b8fef6464a8b7fb
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-24 15:31:49 -0400

    Subtraction Complete

--- Basic-JavaScript-Projects/Project3-math-operators/JS/main.js
+++ Basic-JavaScript-Projects/Project3-math-operators/JS/main.js
@@ -1,4 +1,11 @@
+//ADDITION
 function addFunction() { //Defining the function and assigning it a name
     var addition= 12 + 24; //Defining a variablr and assigning it parameters
     document.getElementById("math").innerHTML= "12 + 24 =" + addition;
+}
+
+//SUBTRACTION
+function subtractFunction() { //Defining the function and assigning it a name
+    var subtraction= 45 - 10; //Defining a variablr and assigning it parameters
+    document.getElementById("subtract").innerHTML= "45 - 10 =" + subtraction;
 }
