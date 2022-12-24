f274fe1b9153b6521876c363e04799496830dafa
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-24 15:45:43 -0400

    Multiplication and Division complete

--- Basic-JavaScript-Projects/Project3-math-operators/JS/main.js
+++ Basic-JavaScript-Projects/Project3-math-operators/JS/main.js
@@ -1,11 +1,23 @@
 //ADDITION
 function addFunction() { //Defining the function and assigning it a name
-    var addition= 12 + 24; //Defining a variablr and assigning it parameters
-    document.getElementById("math").innerHTML= "12 + 24 =" + addition;
+    var addition= 12 + 24; //Defining a variable and assigning it parameters
+    document.getElementById("add").innerHTML= "12 + 24 =" + addition;
 }
 
 //SUBTRACTION
 function subtractFunction() { //Defining the function and assigning it a name
-    var subtraction= 45 - 10; //Defining a variablr and assigning it parameters
+    var subtraction= 45 - 10; //Defining a variable and assigning it parameters
     document.getElementById("subtract").innerHTML= "45 - 10 =" + subtraction;
+}
+
+//MULTIPLICATION
+function multiplyFunction() { //Defining the function and assigning it a name
+    var multiplication= 105 * 35; //Defining a variable and assigning it parameters
+    document.getElementById("multiply").innerHTML= "105 x 35 =" + multiplication;
+}
+
+//DIVISION
+function divideFunction() { //Defining the function and assigning it a name
+    var subtraction= 144 / 12; //Defining a variable and assigning it parameters
+    document.getElementById("divide").innerHTML= "144 / 12 =" + subtraction;
 }
