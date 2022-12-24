99718c0c752193b211060b260a5ad2b0c5b216b7
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-24 15:17:41 -0400

    Math Operators Assignment complete

---
+++ Basic-JavaScript-Projects/Project3-math-operators/JS/main.js
@@ -0,0 +1,4 @@
+function addFunction() { //Defining the function and assigning it a name
+    var addition= 12 + 24; //Defining a variablr and assigning it parameters
+    document.getElementById("math").innerHTML= "12 + 24 =" + addition;
+}
