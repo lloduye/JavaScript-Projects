4a3cf0efec330273260e8642288a4a89f23d8cc1
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-24 03:35:45 -0400

    Updated

--- Basic-JavaScript-Projects/Project2-functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2-functions/JS/main.js
@@ -1,6 +1,6 @@
 function myCode() { // Defining a function and assigning it a name
     var reason="Coding is something that makes a difference."; //Defining a variable and assigning it a value
-    reason +=" When I code, I get this feeling that you I'm solving a problem"; //using the +=operator to concatate the two strings of sentences
+    reason +=" When I code, I get this feeling that I'm solving a problem"; //using the +=operator to concatate the two strings of sentences
     document.getElementById("joinsent").innerHTML=reason; //putting the value of the variable
                                                         //into the HTML with a paragraph element
 }
