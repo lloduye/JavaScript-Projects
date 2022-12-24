83e8d7d2df102a397b5bc0089eecf82b79b08796
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-24 16:05:28 -0400

    Multiple operations complete

--- Basic-JavaScript-Projects/Project3-math-operators/JS/main.js
+++ Basic-JavaScript-Projects/Project3-math-operators/JS/main.js
@@ -1,23 +1,29 @@
 //ADDITION
 function addFunction() { //Defining the function and assigning it a name
     var addition= 12 + 24; //Defining a variable and assigning it parameters
-    document.getElementById("add").innerHTML= "12 + 24 =" + addition;
+    document.getElementById("add").innerHTML= "12 + 24 = " + addition;
 }
 
 //SUBTRACTION
 function subtractFunction() { //Defining the function and assigning it a name
     var subtraction= 45 - 10; //Defining a variable and assigning it parameters
-    document.getElementById("subtract").innerHTML= "45 - 10 =" + subtraction;
+    document.getElementById("subtract").innerHTML= "45 - 10 = " + subtraction;
 }
 
 //MULTIPLICATION
 function multiplyFunction() { //Defining the function and assigning it a name
     var multiplication= 105 * 35; //Defining a variable and assigning it parameters
-    document.getElementById("multiply").innerHTML= "105 x 35 =" + multiplication;
+    document.getElementById("multiply").innerHTML= "105 x 35 = " + multiplication;
 }
 
 //DIVISION
 function divideFunction() { //Defining the function and assigning it a name
     var subtraction= 144 / 12; //Defining a variable and assigning it parameters
-    document.getElementById("divide").innerHTML= "144 / 12 =" + subtraction;
+    document.getElementById("divide").innerHTML= "144 / 12 = " + subtraction;
+}
+
+//MULTIPLE OPERATIONS
+function multipleOPFunction() { //Defining the function and assigning it a name
+    var multipleOP= (20 + 30) * 100 / 50 - 25; //Defining a variable and assigning it parameters
+    document.getElementById("multiple").innerHTML= "(20 + 30) x 100 / 50 - 25 = " + multipleOP;
 }
