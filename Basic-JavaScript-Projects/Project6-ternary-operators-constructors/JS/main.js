01b001cff7e224c115b6ffae48d7b4c238020568
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-27 18:50:39 -0400

    Complete

--- Basic-JavaScript-Projects/Project6-ternary-operators-constructors/JS/main.js
+++ Basic-JavaScript-Projects/Project6-ternary-operators-constructors/JS/main.js
@@ -1,7 +1,7 @@
-function rideFunction() {
-    var Height, Can_ride;
-    Height= document.getElementById("Height").value;
-    Can_ride= (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML=Can_ride +" to ride"
+function voteFunction() {
+    var Height, Can_vote;
+    Age= document.getElementById("Age").value;
+    Can_vote= (Age < 18) ? "You are too young":"You are old enough";
+    document.getElementById("Vote").innerHTML=Can_vote +" to vote"
 
 }
