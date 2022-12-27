0f54eb871db9d4854b9fc4a0eae4ca3a4bb1e028
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-27 15:55:11 -0400

    Infinity Assignment

--- Basic-JavaScript-Projects/Project5-comparisons-type-coercion/JS/main.js
+++ Basic-JavaScript-Projects/Project5-comparisons-type-coercion/JS/main.js
@@ -1,1 +1,14 @@
-document.write("100" + 50)
+document.write("100" + 50) //type coercion
+
+function myFunction1() { //defining a fuctiona and assigning it a name
+    var posInfinity =document.write(3E310) //defining a variable and assigning it a name
+    document.getElementById("infinity").innerHTML=posInfinity  //putting the value of the variable
+                                                                //into the HTML with a paragraph element
+}
+
+
+function myFunction2() { //defining a fuctiona and assigning it a name
+    var negInfinity= document.write(-3E310); //defining a variable and assigning it a name
+    document.getElementById("-infinity").innerHTML=negInfinity  //putting the value of the variable
+                                                                //into the HTML with a paragraph element
+}
