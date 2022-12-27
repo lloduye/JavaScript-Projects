fe1b7e07134bcdf6dde5d48d83c092c24d23c477
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-27 14:50:52 -0400

    Dictionary Assignment complete

---
+++ Basic-JavaScript-Projects/Project4-dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function myFunction1() {
+    var Hkane = {
+            Name:"Harry Kane",
+            Age:28,
+            Sex:"Male",
+            Position:"Striker",
+            Nationality:"England"
+    };
+    document.getElementById("dictionary").innerHTML=Hkane.Nationality
+}
