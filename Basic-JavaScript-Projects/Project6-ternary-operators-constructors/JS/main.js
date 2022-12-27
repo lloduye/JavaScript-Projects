27146d121f6b0f2fe038abdf7d1ccbbc991b74ab
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-27 18:44:00 -0400

    Complete

---
+++ Basic-JavaScript-Projects/Project6-ternary-operators-constructors/JS/main.js
@@ -0,0 +1,7 @@
+function rideFunction() {
+    var Height, Can_ride;
+    Height= document.getElementById("Height").value;
+    Can_ride= (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML=Can_ride +" to ride"
+
+}
