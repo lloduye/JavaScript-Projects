1db84091e4c9e70f88c531b33e4175145c2cbb3e
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-27 14:58:40 -0400

    Project4_Dictionaries Submission Assignment

--- Basic-JavaScript-Projects/Project4-dictionaries/JS/main.js
+++ Basic-JavaScript-Projects/Project4-dictionaries/JS/main.js
@@ -1,10 +1,14 @@
-function myFunction1() {
-    var Hkane = {
-            Name:"Harry Kane",
-            Age:28,
-            Sex:"Male",
-            Position:"Striker",
-            Nationality:"England"
+//KVP Dicionary
+function myFunction1() { //defining the fuction and assigning it a name
+    var Hkane = { //defining a variable and assigning it a value
+            Name:"Harry Kane", //KVP
+            Age:28, //KVP
+            Sex:"Male", //KVP
+            Position:"Striker", //KVP
+            Nationality:"England" //KVP
     };
-    document.getElementById("dictionary").innerHTML=Hkane.Nationality
+    delete Hkane.Nationality; //delete statement to remove the Key before it's value is displayed
+    document.getElementById("dictionary").innerHTML=Hkane.Nationality  //putting the value of the variable
+                                                                     //into the HTML with a paragraph element
 }
+
