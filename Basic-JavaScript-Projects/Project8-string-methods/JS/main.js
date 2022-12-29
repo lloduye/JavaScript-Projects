a2aee7f78f9e046841f8006aaffd2fe2aa9fc7d2
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-29 11:55:30 -0400

    Assignment complete

--- Basic-JavaScript-Projects/Project8-string-methods/JS/main.js
+++ Basic-JavaScript-Projects/Project8-string-methods/JS/main.js
@@ -6,3 +6,22 @@ function fullsentFunction() { //defining a function and assigning it a name
     var wholesent = sent1.concat(sent2,sent3,sent4); //concatenating multiple variables
     document.getElementById("concatenate").innerHTML= wholesent //outputting to the html element
 }
+
+//Slice() Method
+function sliceFunction() { //Defining function and assigning it a name
+    var Sentence="All work and no play makes Johnny a dull boy."; //Defining a variable and assigning it a string
+    var Section= Sentence.slice(27,33); //Defining a variable and assigning it a value
+    document.getElementById("slice").innerHTML=Section
+}
+
+//toString() Function
+function numbtostrFunction() { //Defining a function and assigning it a name
+    var X=182; //Defining a variable and assigning it a value
+    document.getElementById("numbtostr").innerHTML=X.toString();
+}
+
+//toPrecision() Function
+function toprecFunction() { //Defining a function and assigning it a name
+    var X=12938.25686585688659858588; //Defining a variable and assigning it a value
+    document.getElementById("toprec").innerHTML=X.toPrecision();
+}
