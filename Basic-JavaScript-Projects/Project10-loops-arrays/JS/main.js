bc974254125461c7878c5b81049b6880b20af84c
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-12-30 14:38:57 -0400

    For Loop Assingment complete

--- Basic-JavaScript-Projects/Project10-loops-arrays/JS/main.js
+++ Basic-JavaScript-Projects/Project10-loops-arrays/JS/main.js
@@ -8,3 +8,14 @@ function loopFunction() { //Defining a function and assigning it a name
     }
     document.getElementById("loop").innerHTML=Counter; //outoutting the results to the HTML 
 }
+
+//For Loop Assignment
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumphet", "Flute"];
+var Content ="";
+var Y;
+function for_loop() { //Defining a function and assigning it a name
+    for (Y=0; Y<Instruments.length; Y++) { //Conditional statement
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("listofinsts").innerHTML=Content;
+    }
